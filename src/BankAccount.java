e847955bfe41486d3120ec4fb208cc2150b20c13
sandy zulof <sandyzalaf@live.com>
2020-04-22 14:54:30 +0300

    initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,25 @@
+public class BankAccount {
+    // BankAccount attributes
+    private String accountNumber;
+    private String accountName;
+    private double balance;
+
+    public BankAccount(String accNumber, String accName) {
+        accountNumber = accNumber;
+        accountName = accName;
+        balance = 0;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
